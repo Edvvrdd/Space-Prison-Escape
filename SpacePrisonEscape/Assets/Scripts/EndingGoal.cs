be0e03cc1f26f57062d87f426fe42c06b9abdd1a
MatthewHenry18 <78063551+MatthewHenry18@users.noreply.github.com>
2021-08-21 22:23:50 -0400

    Finished Final Game Loop

--- SpacePrisonEscape/Assets/Scripts/EndingGoal.cs
+++ SpacePrisonEscape/Assets/Scripts/EndingGoal.cs
@@ -7,17 +7,40 @@ public class EndingGoal : MonoBehaviour
     //making variable of type player action controls
     private PlayerActionMappings playerControlBindings;
     [SerializeField] private System.String NextLevelSceneName;
+    
+
+    [SerializeField] private bool IsFinalLevel;
+    private bool hasDisplayedEndingText = false;
+
+    [SerializeField] GameObject escapedText;
+    [SerializeField] GameObject ContinueText;
+
+
+    void Awake()
+    {
+        //instanciating the variable 
+        playerControlBindings = new PlayerActionMappings();
+    }
+    //Enabling controls 
+    private void OnEnable()
+    {
+        playerControlBindings.Enable();
+    }
+    private void OnDisable()
+    {
+        playerControlBindings.Disable();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerControlBindings.LandMovement.Jump.performed += _ => Jump();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -30,6 +53,25 @@ public class EndingGoal : MonoBehaviour
             //Left -> Replay
             //Right -> Next Level
             
+          
+            if (IsFinalLevel)
+            {
+                Debug.Log("Render Text");
+                escapedText.SetActive(true);
+                ContinueText.SetActive(true);
+                hasDisplayedEndingText = true;
+            }
+            else
+            {
+                SceneManager.LoadScene(NextLevelSceneName);
+            }
+           
+        }
+    }
+    private void Jump()
+    {
+        if(hasDisplayedEndingText)
+        {
             SceneManager.LoadScene(NextLevelSceneName);
         }
     }
